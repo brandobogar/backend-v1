9b9c733439b1dadfb42513f7c51c2b18f6705a6f
brandobogar <brandobogar@gmail.com>
2023-09-10 15:56:20 +0800

    add new

--- api/index.py
+++ api/index.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
 
+@app.route('/about', methods=['GET'])
+def about():
+    return 'about page'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
